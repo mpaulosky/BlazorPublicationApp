5f06332ceb37743bf665d1a1bab3c1760a5efb9b
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-07-25 07:15:29 -0700

    Add new files

---
+++ BlazorPublication/Entities/Publication.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorPublication.Entities;
+
+public class Publication
+{
+
+	public Guid Id { get; set; }
+
+	public string Name { get; set; } = string.Empty;
+
+	public string SubstackUrl { get; set; } = string.Empty;
+
+	public string NotesUrl { get; set; } = string.Empty;
+
+	public int Subscribers { get; set; }
+
+	public string Category1 { get; set; } = string.Empty;
+
+	public string Category2 { get; set; } = string.Empty;
+
+	public string About { get; set; } = string.Empty;
+
+	public string AboutPublication { get; set; } = string.Empty;
+
+	public string AboutWriter { get; set; } = string.Empty;
+
+}
---
+++ BlazorPublication/Features/Publications/GetPublications.cs
@@ -0,0 +1,39 @@
+﻿using BlazorPublication.Data;
+using BlazorPublication.Entities;
+
+using Mapster;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorPublication.Features.Publications;
+
+public static class GetPublications
+{
+
+	public class Handler(ApplicationDbContext context)
+	{
+
+		public async Task<List<Response>> HandleAsync()
+		{
+
+			List<Publication> publications = await context.Publications.ToListAsync();
+
+			return publications.Adapt<List<Response>>();
+
+		}
+
+	}
+
+	public record Response(
+		Guid Id,
+		string Name,
+		string SubstackUrl,
+		string NotesUrl,
+		int Subscribers,
+		string Category1,
+		string Category2,
+		string AboutPublication,
+		string AboutWriter
+		);
+
+}
